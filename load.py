b61bc261ce070e6b293f4e7489683d70904d14a0
Fenris159 <drew.brdly78@gmail.com>
2026-05-10 20:40:09 -0500

    chore: release v1.6.4 (plugin_version, pyproject, README table)

--- load.py
+++ load.py
@@ -46,7 +46,7 @@ from .ui import UIManager
 
 # Plugin metadata
 plugin_name = os.path.basename(os.path.dirname(__file__))
-plugin_version = "1.6.4-dev"
+plugin_version = "1.6.4"
 # Exposed for EDMC plug.get_version() / Plugin Browser (see PLUGINS.md)
 VERSION = plugin_version
 
